2a84ec6437ccbad7b5452dcbb8faf59b43290079
chandan198 <chandan198@users.noreply.github.com>
2021-07-25 13:08:20 +0530

    practice 2

---
+++ Book.java
@@ -0,0 +1,11 @@
+package in.ashokit;
+
+public class Book {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to my book store");
+		System.out.println("Testing");
+
+	}
+
+}
